9f60faf0d1bfd5e00eaa782101f648644a387724
MeeL1ght <meelight12@gmail.com>
2022-09-12 20:49:37 -0400

    temporary update

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,4 @@
 import Boldor from './../boldor.mjs'
-// import { isNumber } from './../src/utils/is-number.js'
 
 // (!) Run with nodemon script
 const boldor = new Boldor({
@@ -7,6 +6,6 @@ const boldor = new Boldor({
   currency: 12
 })
 
-console.clear() // Don't remove me please hhhhh
-// Output: { currency... }
+console.clear()
+
 console.log(boldor.getProps())
